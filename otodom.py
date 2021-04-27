3e81208541772bb2d73a2afda2a18144a0288e87
adam <wilkadam99@gmail.com>
2021-04-27 19:00:52 +0000

    Db connection

--- otodom.py
+++ otodom.py
@@ -1,6 +1,16 @@
 from bs4 import BeautifulSoup
 from requests import get
 import re
+import sqlite3
+from sys import argv
+
+conn = sqlite3.connect("data.db")
+cursor = conn.cursor()
+
+# Create tables in DB
+if len(argv) > 1 and argv[1] == 'create':
+    cursor.execute('''CREATE TABLE offers (price REAL, rooms REAL, area TEXT, district TEXT, rent REAL, floor REAL, bail REAL)''')
+    quit()
 
 # Iterafe through number of pages
 for page in (1,115):
@@ -20,16 +30,23 @@ for page in (1,115):
 
         get_details = get(link)
         page_details = BeautifulSoup(get_details.content, 'html.parser')
-
         
-        info = str(page_details.find('div', class_='css-1d9dws4 egzohkh2').get_text())
-        #rent = re.search('(?<=Czynsz - dodatkowo:)(\d{3})', info).group()
+        info = str(page_details.find('div', class_='css-1d9dws4 egzohkh2').get_text()).replace(' ','')
+
         try:
-            rent = re.search('(?<=Czynsz - dodatkowo:)(\d{3})', info).group()
-    #    floor = re.search('(?<=Piętro:)(\d)', info).group()
-    #   bail = re.search('(?<=Kaucja:)(\d{1,5}$)', info).group()
+            rent = re.search('(?<=Czynsz-dodatkowo:)(\d{3,4})', info).group()
+            floor = re.search('(?<=Piętro:)(\d{1,2})', info).group()
+            bail = re.search('(?<=Kaucja:)(\d+)', info).group()
         except:
             pass
 
+        print(price, rooms, area, district)
+        # try:
+        #     print("Kaucja: " + bail + " Piętro: " + floor + " Czynsz: " + rent)
+        # except:
+        #     pass
 
-        print(price, rooms, area, district, rent)
+        cursor.execute('INSERT INTO offers VALUES (?,?,?,?,?,?,?)', (price, rooms, area, district, rent, floor, bail))
+        conn.commit()
+
+conn.close()
